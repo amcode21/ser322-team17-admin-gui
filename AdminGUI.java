5eeaf99e8181136d909fcc457d8758d9b8fed268
legowiifun <arath5@gmail.com>
2024-10-03 19:42:36 -0700

    Restore elements that were removed during refactoring

--- AdminGUI.java
+++ AdminGUI.java
@@ -93,6 +93,22 @@ public class AdminGUI {
                     subscriptionDate
                 );
                 break;
+            case 3:
+                System.out.println("Enter artist ID, name, description, and verified status");
+                int artistID=getIntInput(scanner,1,Integer.MAX_VALUE);
+                String artistName=getSanitizedStringInput(scanner);
+                String artistDescription=getSanitizedStringInput(scanner);
+                boolean verified=getBooleanInput(scanner);
+                queries.insertArtist(artistID,artistName,artistDescription,verified);
+                break;
+            case 4:
+                System.out.println("Enter album ID, name, release year, image URL, and artist ID");
+                int albumID=getIntInput(scanner,1,Integer.MAX_VALUE);
+                String albumName=getSanitizedStringInput(scanner);
+                int releaseYear=getIntInput(scanner,1900,2100);
+                String imageURL=getSanitizedStringInput(scanner);
+                int artistID2=getIntInput(scanner,1,Integer.MAX_VALUE);
+                queries.insertAlbum(albumID,albumName,releaseYear,imageURL,artistID2);
             // Additional cases can be added for other entities
             default:
                 System.out.println("Invalid option. Returning to main menu.");
@@ -113,28 +129,51 @@ public class AdminGUI {
         );
         System.out.println("6. Get the number of subscribers");
         System.out.println(
-            "7. Get the number of subscribers who liked a song after subscription"
+                "7. Get the number of subscribers who liked a song after subscription"
         );
+        System.out.println("8. Get the songs that a specific user id liked");
+        System.out.println("9. Get the songs that are in a specific album id");
+        System.out.println("10. Get the songs that had someone like them before a specific date");
 
         int selectOption = getIntInput(scanner, 1, 20);
 
         switch (selectOption) {
             case 1:
-                queries.SelectUsersWithSubscription();
+                queries.selectUsersWithSubscription();
                 break;
             case 2:
-                queries.SelectUsersWithNoSubscription();
+                queries.selectUsersWithoutSubscription();
                 break;
             case 3:
                 System.out.println("Enter the year:");
                 int year = getIntInput(scanner, 1900, 2100);
-                queries.SelectUsersJoinedBeforeYear(year);
+                queries.selectUsersJoinedBeforeYear(year);
                 break;
             case 5:
                 System.out.println("Enter the birthday (yyyy-mm-dd):");
                 String birthday = getValidatedDateInput(scanner);
-                queries.SelectUsersWithBirthday(birthday);
+                queries.selectUsersWithBirthday(birthday);
                 break;
+            case 6:
+                queries.getSubscriberCount();
+                break;
+            case 7:
+                queries.getLikedSongPostSubscription();
+                break;
+            case 8:
+                System.out.println("Enter the user ID");
+                int userID=getIntInput(scanner,1,Integer.MAX_VALUE);
+                queries.selectSongsLikedByUser(userID);
+                break;
+            case 9:
+                System.out.println("Enter the album ID");
+                int albumID=getIntInput(scanner,1,Integer.MAX_VALUE);
+                queries.selectSongsByAlbum(albumID);
+                break;
+            case 10:
+                System.out.println("Enter the date");
+                String date=getValidatedDateInput(scanner);
+                queries.selectSongsLikedBeforeDate(date);
             // Additional cases can be added for other select options
             default:
                 System.out.println("Invalid input. Returning to main menu.");
@@ -148,6 +187,8 @@ public class AdminGUI {
         System.out.println("2. Subscription");
         System.out.println("3. Artist");
         System.out.println("4. Album");
+
+        //TODO: other fields. See queries code
 
         int updateOption = getIntInput(scanner, 1, 8);
 
@@ -182,6 +223,95 @@ public class AdminGUI {
                         break;
                 }
                 break;
+            case 2:
+                System.out.println("Enter subscription ID:");
+                int subscriptionID = getIntInput(scanner, 1, Integer.MAX_VALUE);
+                System.out.println(
+                        "Select value to update: 1. Billing Details, 2. Subscription Date"
+                );
+                int subscriptionField = getIntInput(scanner, 1, 2);
+                switch(subscriptionField) {
+                    case 1:
+                        System.out.println("Enter new billing details:");
+                        String billingDetails = getSanitizedStringInput(scanner);
+                        queries.updateBillingDetails(billingDetails, subscriptionID);
+                        break;
+                    case 2:
+                        System.out.println("Enter new subscription date (yyyy-mm-dd):");
+                        String subscriptionDate = getValidatedDateInput(scanner);
+                        queries.updateSubscriptionDate(subscriptionDate, subscriptionID);
+                        break;
+                    default:
+                        System.out.println(
+                                "Invalid input. Returning to the main menu."
+                        );
+                        break;
+                }
+                break;
+            case 3:
+                System.out.println("Enter artist ID:");
+                int artistID = getIntInput(scanner, 1, Integer.MAX_VALUE);
+                System.out.println(
+                        "Select value to update: 1. name, 2. description, 3. verified status"
+                );
+                int artistField = getIntInput(scanner, 1, 3);
+                switch (artistField) {
+                    case 1:
+                        System.out.println("Enter new name:");
+                        String name = getSanitizedStringInput(scanner);
+                        queries.updateArtistName(name, artistID);
+                        break;
+                    case 2:
+                        System.out.println("Enter new description:");
+                        String description = getSanitizedStringInput(scanner);
+                        queries.updateArtistDescription(description, artistID);
+                        break;
+                    case 3:
+                        System.out.println("Enter new verified status:");
+                        boolean verifiedStatus = getBooleanInput(scanner);
+                        queries.updateArtistVerificationStatus(verifiedStatus, artistID);
+                        break;
+                    default:
+                        System.out.println(
+                                "Invalid input. Returning to the main menu."
+                        );
+                        break;
+                }
+                break;
+            case 4:
+                System.out.println("Enter album ID:");
+                int albumID = getIntInput(scanner, 1, Integer.MAX_VALUE);
+                System.out.println(
+                        "Select value to update: 1. name, 2. release year, 3. image URL, 4. artist ID"
+                );
+                int albumField = getIntInput(scanner, 1, 4);
+                switch (albumField) {
+                    case 1:
+                        System.out.println("Enter new name:");
+                        String name = getSanitizedStringInput(scanner);
+                        queries.updateAlbumName(name, albumID);
+                        break;
+                    case 2:
+                        System.out.println("Enter new release year:");
+                        int releaseYear = getIntInput(scanner,1900,2100);
+                        queries.updateAlbumYear(releaseYear, albumID);
+                        break;
+                    case 3:
+                        System.out.println("Enter new image URL:");
+                        String imgURL = getSanitizedStringInput(scanner);
+                        queries.updateAlbumImage(imgURL, albumID);
+                        break;
+                    case 4:
+                        System.out.println("Enter new artist ID:");
+                        int artistIDForAlbum = getIntInput(scanner,1,Integer.MAX_VALUE);
+                        queries.updateAlbumArtist(artistIDForAlbum, albumID);
+                        break;
+                    default:
+                        System.out.println(
+                                "Invalid input. Returning to the main menu."
+                        );
+                        break;
+                }
                 //TODO: fill in for other fields
             default:
                 System.out.println("Invalid option. Returning to main menu.");
@@ -196,7 +326,7 @@ public class AdminGUI {
         System.out.println("3. Artist");
         System.out.println("4. Album");
 
-        int deleteOption = getIntInput(scanner, 1, 4);
+        int deleteOption = getIntInput(scanner, 1, 8);
 
         switch (deleteOption) {
             case 1:
@@ -204,6 +334,18 @@ public class AdminGUI {
                 int userId = getIntInput(scanner, 1, Integer.MAX_VALUE);
                 queries.deleteUser(userId);
                 break;
+            case 2:
+                System.out.println("Enter subscription ID:");
+                int subscriptionID = getIntInput(scanner, 1, Integer.MAX_VALUE);
+                queries.deleteSubscription(subscriptionID);
+            case 3:
+                System.out.println("Enter artist ID:");
+                int artistID = getIntInput(scanner, 1, Integer.MAX_VALUE);
+                queries.deleteArtist(artistID);
+            case 4:
+                System.out.println("Enter album ID:");
+                int albumID = getIntInput(scanner, 1, Integer.MAX_VALUE);
+                queries.deleteAlbum(albumID);
             // Additional cases for other delete options
             default:
                 System.out.println("Invalid option. Returning to main menu.");
@@ -255,6 +397,21 @@ public class AdminGUI {
                 System.out.println(
                     "Invalid date format. Please enter a date in the format 'yyyy-mm-dd':"
                 );
+            }
+        }
+    }
+
+    private static boolean getBooleanInput(Scanner scanner) {
+        boolean input;
+        while (true) {
+            if (scanner.hasNextBoolean()) {
+                input = scanner.nextBoolean();
+                return input;
+            } else {
+                System.out.println(
+                        "Invalid input. Please enter a valid number:"
+                );
+                scanner.next(); // Clear the invalid input
             }
         }
     }
